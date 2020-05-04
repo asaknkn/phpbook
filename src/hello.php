346203f809d7b8181cd3f7973f627e75ac0583d9
Kentarou Asai <asaknkn@gmail.com>
2020-05-04 16:02:00 +0900

    first commit

---
+++ src/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Hello</title>
+</head>
+<body>
+<p>Let's PHP--
+<?php
+  print "Hello World!\n";
+?>
+</p>
+</body>
---
+++ src/receivename.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Receivename</title>
+</head>
+<body>
+  <?php
+    print ($_POST['name']);
+  ?>
+  さん、こんにちわ。
+</body>
+</html>
